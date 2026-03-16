2537ecdc2ef70f9681e869afd53a88878c3aa754
henriquet31 <henriquet023@outlook.com>
2026-03-16 19:43:03 -0300

    refatorar: integrar funcionalidade de remoção de tarefas e ajustar estrutura de estado

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -1,35 +1,43 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import tarefa from '../../models/Tarefa'
 import * as enums from '../../utils/enums/Tarefa'
+import Tarefa from '../../models/Tarefa'
 
+type TarefasState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      descricao: 'Estudar JavaScript revendo o exercício dp módulo 7',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar JavaScript'
+    },
+    {
+      id: 2,
+      descricao: 'Estudar material de apoio',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar TypeScript'
+    },
+    {
+      id: 3,
+      descricao: 'Praticar a construção de uma landing page',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar Bootstrap'
+    }
+  ]
+}
 const tarefasSlice = createSlice({
   name: 'tarefas',
-  initialState: [
-    new tarefa(
-      'Estudar JavaScript',
-      enums.Prioridade.IMPORTANTE,
-      enums.Status.PENDENTE,
-      '',
-      1
-    ),
-    new tarefa(
-      'Estudar TypeScript',
-      enums.Prioridade.URGENTE,
-      enums.Status.CONCLUIDA,
-      'Rever aula 2 do módulo',
-      2
-    ),
-    new tarefa(
-      'Estudar React',
-      enums.Prioridade.URGENTE,
-      enums.Status.PENDENTE,
-      'Praticar o useEffect',
-      3
-    )
-  ],
+  initialState,
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state = state.filter((tarefa) => tarefa.id !== action.payload)
+      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
     }
   }
 })
